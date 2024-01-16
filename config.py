f66eba49c94cd4f4df628e177a75c43269d57a62
Nagim123 <fourcolorsgame@gmail.com>
2024-01-16 20:27:46 +0500

    create -> load

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config():
     __data: dict = None
 
     @staticmethod
-    def create(config_file_path: str) -> None:
+    def load(config_file_path: str) -> None:
         config_file = open(config_file_path, "r")
         Config.__data = json.loads(config_file.read())
         config_file.close()
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from config import Config
 from bot import run_bot
 
 if __name__ == "__main__":
-    Config.create("config.json")
+    Config.load("config.json")
     asyncio.run(run_bot(Config.data()["DISCORD_BOT_TOKEN"]))
