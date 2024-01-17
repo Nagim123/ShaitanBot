b6b71b8d94897f3a71d96d0d60ddbc7cef5fd8c2
Nagim123 <fourcolorsgame@gmail.com>
2024-01-17 16:38:19 +0500

    Initial start for scheduling system

--- cogs/pinterest_scheduler.py
+++ cogs/pinterest_scheduler.py
@@ -1,24 +1,53 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+import os
+
 from discord.ext import commands
 from random import random
+from pinterest_board_parser.pinterest_board import PinterestBoard
+from ..pin_scheduling.pinterest_board_scheduler import PinterestBoardScheduler
+from ..config import Config
 
 if TYPE_CHECKING:
     from bot import ShaitanBot
 
 class PinterestScheduler(commands.Cog):
-
     def __init__(self, bot: ShaitanBot) -> None:
         self.bot = bot
+        self.board_cache_folder_path = Config.data()["PINTEREST_BOARD_CACHE_FOLDER"]
+        self.schedule_cache_folder_path = Config.data()["PINTEREST_BOARD_SCHEDULE_CACHE_FOLDER"]
+        self.channel_schedules: dict[int, list[tuple[str, PinterestBoardScheduler]]] = dict()
     
     @commands.command()
-    async def add_pin_board(self, ctx: commands.Context) -> None:
+    async def add_pin_board(self, ctx: commands.Context, board_owner: str, board_name: str) -> None:
+        board = PinterestBoard(board_owner, board_name, cache_file_path=f"{self.board_cache_folder_path}/{board_owner}_{board_name}")
+        
+        if ctx.channel.id in self.channel_schedules:
+            scheduler = PinterestBoardScheduler(board, f"{self.board_cache_folder_path}/{ctx.channel.id}/{board_owner}_{board_name}")
+            self.channel_schedules[ctx.channel.id].append((f"{board_owner}_{board_name}", scheduler))
+        else:
+            os.mkdir(f"{self.board_cache_folder_path}/{ctx.channel.id}")
+            scheduler = PinterestBoardScheduler(board, f"{self.board_cache_folder_path}/{ctx.channel.id}/{board_owner}_{board_name}")
+            self.channel_schedules[ctx.channel.id] = [(f"{board_owner}_{board_name}", scheduler)]
+        
         await ctx.reply("Доска успешно добавлена!")
 
     @commands.command()
-    async def remove_pin_board(self, ctx: commands.Context) -> None:
-        pass
+    async def remove_pin_board(self, ctx: commands.Context, board_owner: str, board_name: str) -> None:
+        if ctx.channel.id in self.channel_schedules:
+            await ctx.reply("В этом канале нет ни одной доски.")
+        else:
+            if os.path.exists(f"{self.board_cache_folder_path}/{ctx.channel.id}/{board_owner}_{board_name}"):
+                scheduler_index = -1
+                for index, scheduler in enumerate(self.channel_schedules[ctx.channel.id]):
+                    if scheduler[0] == f"{board_owner}_{board_name}":
+                        scheduler_index = index
+                self.channel_schedules[ctx.channel.id].pop(scheduler_index)
+                os.remove(f"{self.board_cache_folder_path}/{ctx.channel.id}/{board_owner}_{board_name}")
+                await ctx.reply(f"Доска {board_name} успешно удалена из данного канала!")
+            else:
+                await ctx.reply("Такая доска не прикрепленна к этому каналу.")
 
     @commands.command()
     async def show_schedule(self, ctx: commands.Context) -> None:
---
+++ pin_scheduling/pinterest_board_scheduler.py
@@ -0,0 +1,50 @@
+import os
+from random import shuffle, randint
+from pinterest_board_parser.pinterest_board import PinterestBoard
+from pinterest_board_parser.pinterest_pin import PinterestPin
+
+class PinterestBoardScheduler:
+    def __init__(self, board: PinterestBoard, save_file_path: str) -> None:
+        self.__board = board
+        self.__save_file_path = save_file_path
+
+        if os.path.exists(save_file_path):
+            self.__load_from_file()
+        else:
+            self.__pin_count = len(board.get_pins())
+            self.__scheduled_pin_indexes = [i for i in range(self.__pin_count)]
+            shuffle(self.__scheduled_pin_indexes)
+            self.__current_pin_index = 0
+            self.__save_to_file()
+
+    def get_next_pin(self) -> PinterestPin:
+        if self.__current_pin_index >= len(self.__scheduled_pin_indexes):
+            raise Exception("All pins were shown!")
+        
+        pin_index = self.__scheduled_pin_indexes[self.__current_pin_index]
+        parsed_pins = self.__board.get_pins()
+        
+        for i in range(self.__pin_count, parsed_pins):
+            self.__scheduled_pin_indexes.insert(randint(0, self.__pin_count-1), i)
+        self.__current_pin_index += 1
+        
+        self.__save_to_file()
+        return parsed_pins[pin_index]
+
+    def __load_from_file(self) -> None:
+        with open(self.__save_file_path, "r") as save_file:
+            data = save_file.read().split('\n')
+            self.__pin_count = int(data[0])
+            self.__current_pin_index = int(data[1])
+            self.__scheduled_pin_indexes = [int(x) for x in data[2].split(',')]
+
+    def __save_to_file(self) -> None:
+        with open(self.__save_file_path, "w") as save_file:
+            save_file.write(str(self.__pin_count))
+            save_file.write(str(self.__current_pin_index))
+            save_file.write(",".join(self.__scheduled_pin_indexes) + '\n')
+    
+    def __eq__(self, __value: object) -> bool:
+        if isinstance(__value, PinterestBoardScheduler):
+            return self.__save_file_path == __value.__save_file_path
+        return False
