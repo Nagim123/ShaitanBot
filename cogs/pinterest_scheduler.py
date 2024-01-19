195ed4157bb6e54b8351eb506dca838816d15277
Nagim123 <fourcolorsgame@gmail.com>
2024-01-19 14:17:27 +0300

    Add pinterest logging!

--- cogs/pinterest_scheduler.py
+++ cogs/pinterest_scheduler.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import os
+import logging
 
 from discord.ext import commands, tasks
 from random import random
@@ -27,12 +28,17 @@ class PinterestScheduler(commands.Cog):
         self.__board_pool = PinterestBoardPool()
         self.__channel_schedulers: dict[int, PinteresetChannelScheduler] = dict()
         self.__schedule_cache_folder_path = Config.data()["PINTEREST_BOARD_SCHEDULE_CACHE_FOLDER"]
-        
+        self.__logger = logging.getLogger("SchedulingLogger")
+        self.__logger.addHandler(logging.FileHandler("./logs/pin_logger.txt"))
+
         self.__load_all_board_schedulers_from_cache()
         self.__send_pins_to_channels_on_time.start()
+        self.__logger.info("Pinterest module loaded succesfully!")
     
     @commands.command()
     async def add_pin_board(self, ctx: commands.Context, board_owner: str, board_name: str) -> None:
+        """Add board to current channel"""
+        self.__logger.info(f"/add_pin_board {board_owner} {board_name} command was entered")
         try:
             board = self.__board_pool.link_board(board_owner, board_name)
             if ctx.channel.id in self.__channel_schedulers:
@@ -43,53 +49,68 @@ class PinterestScheduler(commands.Cog):
                 self.__channel_schedulers[ctx.channel.id] = channel_scheduler
         except BoardAlreadyAddedException as e:
             await ctx.reply(e.get_user_format_error())
+            self.__logger.info(f"Board {board_name} already added to channel")
             return
         except PinterestBoardDoesNotExistException:
             await ctx.reply("Такой доски не существует.")
+            self.__logger.info(f"Board {board_name} was not found")
             return
 
+        self.__logger.info(f"Board {board_name} added succesfully")
         await ctx.reply("Доска успешно добавлена!")
 
     @commands.command()
     async def remove_pin_board(self, ctx: commands.Context, board_owner: str, board_name: str) -> None:
+        """Remove board from current channel"""
+        self.__logger.info(f"/remove_pin_board {board_owner} {board_name} command was entered")
         if not ctx.channel.id in self.__channel_schedulers:
+            self.__logger.info(f"No boards in channel {ctx.channel.id}")
             await ctx.reply("К этому каналу не прикрепленна ни одна доска.")
             return
-        
         try:
             self.__channel_schedulers[ctx.channel.id].remove_pin_board(board_owner, board_name)
             self.__board_pool.unlink_board(board_owner, board_name)
+            self.__logger.info(f"Board {board_name} removed succesfully from {ctx.channel.id}")
         except BoardNotFoundException as e:
+            self.__logger.info(f"Board {board_name} was not found in {ctx.channel.id}")
             await ctx.reply(e.get_user_format_error())
             return
 
         await ctx.reply(f"Доска {board_name} успешно удалена из данного канала!")
 
-    @commands.command()
-    async def show_schedule(self, ctx: commands.Context) -> None:
-        await ctx.reply(f"WHAT TEST")
+    # @commands.command()
+    # async def show_schedule(self, ctx: commands.Context) -> None:
+    #     await ctx.reply(f"NOT IMPLEMENTED")
 
-    @commands.command()
-    async def change_scheduling(self, ctx: commands.Context, new_scheduling: str) -> None:
-        await ctx.reply(f"NOT IMPLEMENTED")
+    # @commands.command()
+    # async def change_scheduling(self, ctx: commands.Context, new_scheduling: str) -> None:
+    #     await ctx.reply(f"NOT IMPLEMENTED")
     
     @commands.command()
     async def change_send_time(self, ctx: commands.Context, new_hour: int) -> None:
+        """Changes time when the bot sends pic or video"""
+        self.__logger.info(f"/change_send_time {new_hour} command was entered")
         if ctx.channel.id in self.__channel_schedulers:
             self.__channel_schedulers[ctx.channel.id].change_accept_time_hour(new_hour)
             await ctx.reply(f"Время отправки ежедневного пина изменено на {new_hour}:00")
+            self.__logger.info(f"Send time changed to {new_hour}:00 for channel {ctx.channel.id} successfully!")
             return
         await ctx.reply(f"Добавьте хотя бы 1 доску, чтобы изменять время отправки.")
+        self.__logger.info(f"No boards in channel {ctx.channel.id}")
 
     @tasks.loop(time=EACH_HOUR_TIMES)
     async def __send_pins_to_channels_on_time(self) -> None:
+        self.__logger.info("Time to broadcast pins!")
         for channel in self.__channel_schedulers:
             try:
                 pin = self.__channel_schedulers[channel].get_scheduled_pin()
+                self.__logger.info(f"Send pin {pin.pin_id} to {channel} channel")
                 discord_channel = await self.__bot.fetch_channel(channel)
                 await discord_channel.send(pin.resource_link)
+                self.__logger.info("Send operation was succesfull")
             except NoMorePinsException:
                 discord_channel = await self.__bot.fetch_channel(channel)
+                self.__logger.info(f"No pins left for channel {channel}")
                 await discord_channel.send("Вы просмотрели все пины из всех досок. Добавьте новые)")
             except WrongSchedulingTimeException:
                 continue
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import asyncio
+import logging
 from config import Config
 from bot import run_bot
 
 if __name__ == "__main__":
     Config.load("config.json")
+    logging.basicConfig(level=logging.DEBUG)
     asyncio.run(run_bot(Config.data()["DISCORD_BOT_TOKEN"]))
